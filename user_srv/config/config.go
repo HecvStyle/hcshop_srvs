d33f923b27181e67300d856afdd38666fea1d885
hecv <hecvstyle@foxmail.com>
2021-08-19 17:15:19 +0800

    添加注册中心和服务健康检查处理

--- user_srv/config/config.go
+++ user_srv/config/config.go
@@ -8,8 +8,15 @@ type MySqlConfig struct {
 	User     string `mapstructure:"user" json:"user"`
 }
 
+type ConsulConfig struct {
+	Host string `mapstructure:"host" json:"host"`
+	Port int    `mapstructure:"port" json:"port"`
+}
+
 type ServerConfig struct {
-	Name      string      `mapstructure:"name"`
-	Port      int         `mapstructure:"port"`
-	MysqlInfo MySqlConfig `mapstructure:"mysql" json:"mysql"`
+	Host       string       `mapstructure:"host" json:"host"`
+	Name       string       `mapstructure:"name"`
+	Port       int          `mapstructure:"port"`
+	MysqlInfo  MySqlConfig  `mapstructure:"mysql" json:"mysql"`
+	ConsulInfo ConsulConfig `mapstructure:"consul" json:"consul"`
 }
--- user_srv/main.go
+++ user_srv/main.go
@@ -3,13 +3,16 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/hashicorp/consul/api"
 	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/health"
+	"google.golang.org/grpc/health/grpc_health_v1"
+	"hcshop_srvs/user_srv/global"
 	"hcshop_srvs/user_srv/handler"
 	"hcshop_srvs/user_srv/initialize"
 	"hcshop_srvs/user_srv/proto"
 	"net"
-
-	"google.golang.org/grpc"
 )
 
 func main() {
@@ -34,6 +37,38 @@ func main() {
 	lis, err := net.Listen("tcp", fmt.Sprintf("%s:%d", *IP, *Port))
 	if err != nil {
 		panic("failed to listen:" + err.Error())
+	}
+
+	// 注册健康检查
+	grpc_health_v1.RegisterHealthServer(server, health.NewServer())
+
+	cfg := api.DefaultConfig()
+	cfg.Address = fmt.Sprintf("%s:%d", global.ServerConfig.ConsulInfo.Host, global.ServerConfig.ConsulInfo.Port)
+
+	client, err := api.NewClient(cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	check := &api.AgentServiceCheck{
+		GRPC:                           fmt.Sprintf("192.168.1.75:50051"),
+		Interval:                       "5s",
+		Timeout:                        "5s",
+		DeregisterCriticalServiceAfter: "10s",
+	}
+
+	registration := new(api.AgentServiceRegistration)
+	registration.Name = global.ServerConfig.Name
+	registration.ID = global.ServerConfig.Name
+	registration.Port = *Port
+	registration.Tags = []string{"user-srv"}
+	// 这里别瞎鸡毛加 "http://" scheme，这可是rpc 协议，踩坑 +1
+	registration.Address = fmt.Sprintf("192.168.1.75")
+	registration.Check = check
+
+	err = client.Agent().ServiceRegister(registration)
+	if err != nil {
+		panic(err)
 	}
 
 	err = server.Serve(lis)
