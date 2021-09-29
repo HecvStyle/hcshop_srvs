70684a30d15723737f21fb640cdd770408ebe640
hecv <hecvstyle@foxmail.com>
2021-09-29 21:53:51 +0800

    订单服务
    添加订单服务相关接口

--- order_srv/handler/order.go
+++ order_srv/handler/order.go
@@ -2,7 +2,11 @@ package handler
 
 import (
 	"context"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/emptypb"
+	"hcshop_srvs/inventory_srv/global"
+	"hcshop_srvs/order_srv/model"
 	"hcshop_srvs/order_srv/proto"
 )
 
@@ -10,34 +14,141 @@ type OrderServer struct {
 	proto.UnimplementedOrderServer
 }
 
-func (o *OrderServer) CartItemList(ctx context.Context, req *proto.UserInfo ) (*proto.CartItemListResponse, error) {
-	panic("implement me")
+func (o *OrderServer) CartItemList(ctx context.Context, req *proto.UserInfo) (*proto.CartItemListResponse, error) {
+	var shopCarts []model.ShoppingCart
+	rsp := proto.CartItemListResponse{}
+	if result := global.DB.Where(model.ShoppingCart{User: req.Id}).Find(&shopCarts); result.Error != nil {
+		return nil, result.Error
+	} else {
+		rsp.Total = int32(result.RowsAffected)
+	}
+	for _, cart := range shopCarts {
+		rsp.Data = append(rsp.Data, &proto.ShopCartInfoResponse{
+			Id:      cart.ID,
+			UserId:  cart.User,
+			GoodsId: cart.Goods,
+			Nums:    cart.Nums,
+			Checked: cart.Checked,
+		})
+	}
+	return &rsp, nil
+
 }
 
-func (o *OrderServer) CreateCartItem(ctx context.Context, in *proto.CartItemRequest) (*proto.ShopCartInfoResponse, error) {
-	panic("implement me")
+func (o *OrderServer) CreateCartItem(ctx context.Context, req *proto.CartItemRequest) (*proto.ShopCartInfoResponse, error) { //
+	//先看存在与否，没有就添加，有就添加数量
+	var shopCart model.ShoppingCart
+	if result := global.DB.Where(model.ShoppingCart{Goods: req.GoodsId, User: req.UserId}).Find(&shopCart); result.RowsAffected == 1 {
+		shopCart.Nums += 1
+	} else {
+		shopCart.Nums = 1
+		shopCart.Goods = req.GoodsId
+		shopCart.User = req.UserId
+		shopCart.Checked = false
+	}
+	global.DB.Save(&shopCart)
+	return &proto.ShopCartInfoResponse{Id: shopCart.ID}, nil
+
 }
 
-func (o *OrderServer) UpdateCartItem(ctx context.Context, in *proto.CartItemRequest) (*emptypb.Empty, error) {
-	panic("implement me")
+func (o *OrderServer) UpdateCartItem(ctx context.Context, req *proto.CartItemRequest) (*emptypb.Empty, error) {
+	var shopCart model.ShoppingCart
+	if result := global.DB.Where("goods=? and user=?", req.GoodsId, req.UserId).First(&shopCart); result.RowsAffected == 0 {
+		return nil, status.Errorf(codes.NotFound, "购物车记录不存在")
+	}
+	shopCart.Checked = req.Checked
+	if req.Nums > 0 {
+		shopCart.Nums += req.Nums
+	}
+	global.DB.Save(&shopCart)
+	return &emptypb.Empty{}, nil
 }
 
-func (o *OrderServer) DeleteCartItem(ctx context.Context, in *proto.CartItemRequest) (*emptypb.Empty, error) {
-	panic("implement me")
+func (o *OrderServer) DeleteCartItem(ctx context.Context, req *proto.CartItemRequest) (*emptypb.Empty, error) {
+	if result := global.DB.Where("goods=? and user=?", req.GoodsId, req.UserId).Delete(&model.ShoppingCart{}); result.RowsAffected == 0 {
+		return nil, status.Errorf(codes.NotFound, "购物车记录不存在")
+	}
+	return &emptypb.Empty{}, nil
 }
 
-func (o *OrderServer) CreateOrder(ctx context.Context, in *proto.OrderRequest) (*proto.OrderInfoResponse, error) {
+func (o *OrderServer) CreateOrder(ctx context.Context, req *proto.OrderRequest) (*proto.OrderInfoResponse, error) {
 	panic("implement me")
+	/*
+	1.商品价格自己查询->商品服务 ====>  跨微服务调用
+	2.库存扣减 -> 库存服务 ====> 跨微服务调用
+	 */
+
+
 }
 
-func (o *OrderServer) OrderList(ctx context.Context, in *proto.OrderFilterRequest) (*proto.OrderListResponse, error) {
-	panic("implement me")
+func (o *OrderServer) OrderList(ctx context.Context, req *proto.OrderFilterRequest) (*proto.OrderListResponse, error) {
+	var resp proto.OrderListResponse
+	var orders []model.OrderInfo
+	var total int64
+	global.DB.Where(&model.OrderInfo{User: req.UserId}).Count(&total)
+
+	resp.Total = int32(total)
+	global.DB.Scopes(Paginate(int(req.Pages), int(req.PagePerNums))).Find(&orders)
+
+	for _, order := range orders {
+		resp.Data = append(resp.Data, &proto.OrderInfoResponse{
+			Id:      order.ID,
+			UserId:  order.User,
+			OrderSn: order.OrderSn,
+			PayType: order.PayType,
+			Status:  order.Status,
+			Post:    order.Post,
+			Total:   order.OrderMount,
+			Address: order.Address,
+			Name:    order.SignerName,
+			Mobile:  order.SingerMobile,
+		})
+	}
+	return &resp, nil
 }
 
-func (o *OrderServer) OrderDetail(ctx context.Context, in *proto.OrderRequest) (*proto.OrderInfoDetailResponse, error) {
-	panic("implement me")
+func (o *OrderServer) OrderDetail(ctx context.Context, req *proto.OrderRequest) (*proto.OrderInfoDetailResponse, error) {
+	var resp proto.OrderInfoDetailResponse
+	var order model.OrderInfo
+	if result := global.DB.Where(&model.OrderInfo{BaseModel: model.BaseModel{
+		ID: req.Id,
+	}, User: req.UserId}).Find(&order); result.RowsAffected == 0 {
+		return nil, status.Errorf(codes.NotFound, "订单不存在")
+	}
+
+	orderInfo := proto.OrderInfoResponse{
+		Id:      order.ID,
+		UserId:  order.User,
+		OrderSn: order.OrderSn,
+		PayType: order.PayType,
+		Status:  order.Status,
+		Post:    order.Post,
+		Total:   order.OrderMount,
+		Address: order.Address,
+		Name:    order.SignerName,
+		Mobile:  order.SingerMobile,
+	}
+	resp.OrderInfo = &orderInfo
+
+	var orderGoods []model.OrderGoods
+	if result := global.DB.Where(&model.OrderGoods{Order: order.ID}).Find(&orderGoods); result.Error != nil {
+		return nil, result.Error
+	}
+	for _, goods := range orderGoods {
+		resp.Goods = append(resp.Goods, &proto.OrderItemResponse{
+			GoodsId:    goods.Goods,
+			GoodsName:  goods.GoodsName,
+			GoodsImage: goods.GoodsImage,
+			GoodsPrice: goods.GoodsPrice,
+			Nums:       goods.Nums,
+		})
+	}
+	return &resp, nil
 }
 
-func (o *OrderServer) UpdateOrderStatus(ctx context.Context, in *proto.OrderStatus) (*emptypb.Empty, error) {
-	panic("implement me")
+func (o *OrderServer) UpdateOrderStatus(ctx context.Context, req *proto.OrderStatus) (*emptypb.Empty, error) {
+	if result := global.DB.Model(&model.OrderInfo{}).Where("order_sn = ?", req.OrderSn).Update("status", req.Status); result.RowsAffected == 0 {
+		return nil, status.Errorf(codes.NotFound, "订单不存在")
+	}
+	return &emptypb.Empty{}, nil
 }
